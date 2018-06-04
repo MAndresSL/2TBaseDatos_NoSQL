c3c413061ccd1a18202fab4f5d8af92b6a7321ff
calvinespinoza <calvin_espinoza@hotmail.com>
2018-06-03 21:27:02 -0600

    successully added

---
+++ public/config.js
@@ -0,0 +1,12 @@
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCRNvIRG2ddROhFpBBqvcVNTtKkrhfTwmc",
+    authDomain: "basenosql-fd92c.firebaseapp.com",
+    databaseURL: "https://basenosql-fd92c.firebaseio.com",
+    projectId: "basenosql-fd92c",
+    storageBucket: "basenosql-fd92c.appspot.com",
+    messagingSenderId: "49462684804"
+};
+firebase.initializeApp(config);
